4844f4ff15dfaa73688782e7ae6c7b91c6cdd787
Himanshu Kr Gupta <hkgupta5353@gmail.com>
2026-03-04 15:19:22 +0530

    Initial setup for Techaura 2026

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+return (
+<div className="flex h-screen items-center justify-center bg-gray-900">
+<h1 className="text-5xl font-bold text-blue-400">
+Welcome to Techaura 2026!
+</h1>
+</div>
+)
+}
+
+export default App
